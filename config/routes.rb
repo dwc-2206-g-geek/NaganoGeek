5a9f2a8c24170e935bbb55157c9b6a4d15fcf923
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-07-17 11:22:40 +0000

    cart_itemのコントローラ、モデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  namespace :public do
+    get 'cart_items/index'
+  end
+# # 顧客用
+# # URL /customers/sign_in ...
+# devise_for :customers,skip: [:passwords], controllers: {
+#   registrations: "public/registrations",
+#   sessions: 'public/sessions'
+# }
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+# # 管理者用
+# # URL /admin/sign_in ...
+# devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+#   sessions: "admin/sessions"
+# }
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
