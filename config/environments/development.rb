1a2ed2b85ac2bd8f62ccb97ed3c874097fd5ff93
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-07-17 07:53:33 +0000

    host権限の変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "bc3940f856aa4bb5a923f9c774a98252.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
