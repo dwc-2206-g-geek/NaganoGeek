bf3b2291c5cd6f8ff3679716f66f6a57e8d482ab
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-07-18 07:54:24 +0000

    cart_itemコントローラ内のアクションを定義、モデルにアソシエーション、アクションを記載

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,46 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+
+
   def index
+    @cart_item = Cartitem.new   #この記述はitem_detailのページにも記載,新規投稿のために使用
+    @cart_items = current_user.cart_items #ログイン中のユーザに結びついたカートアイテムが欲しい
+    @total = 0   #合計金額の算出に使用する変数
   end
-  
-  def update
+
+  def create #このアクション内でupdateとcreateにアクションを分岐する。もしデータがあったら、update,なかったらcreate
+    @cart_item = Cartitem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if current_customer.cart_items.find_by(item_id :cart_item_params[:item_id] ) #カート内に同一商品が存在するか調べる
+    # もしカート内にデータがあったなら、同一商品を足してアップデート
+      @cart_item.amount = @cart_item.amount + (cart_item_params[:amount]).to_i
+      @cart_item.save
+      redirect_to cart_items_path #投稿したらカートに遷移？
+    else
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
-  
-  def create
+
+  def update #カート内での数量変更処理
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to request.referer
   end
-  
+
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to current_cart
   end
-  
+
   def destroy_all
+    current_user.cart_items.destroy_all
+    redirect_to request.referer
   end
-  
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,3 +1,14 @@
 class CartItem < ApplicationRecord
+  belongs_to :user 
+  belongs_to :cart
   
+  # 税込み単価の算出,item.rbに記載が好ましい
+  def with_tax_price
+      (price * 1.1).floor
+  end
+  
+  # 小計の算出
+  def subtotal
+      item.with_tax_price * amount
+  end
 end
