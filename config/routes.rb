e2d9e818fd4e853f44ec8ae5615873866a663dd8
tako0820 <105572635+tako0820@users.noreply.github.com>
2022-07-18 08:35:14 +0000

    [genre]view,コントローラー,モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -30,6 +24,7 @@ devise_for :customers,skip: [:passwords], controllers: {
 
   namespace :admin do
     resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
