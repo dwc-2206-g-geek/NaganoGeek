49ddccc42a8e6c2891b4c8fa8d256d1393ea9c4c
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-07-18 04:29:37 +0000

    cart_item内にアクションの外枠を作成、ルーティングの記述を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   
-  namespace :public do
-    get 'cart_items/index'
-  end
-# # 顧客用
-# # URL /customers/sign_in ...
-# devise_for :customers,skip: [:passwords], controllers: {
-#   registrations: "public/registrations",
-#   sessions: 'public/sessions'
-# }
 
-# # 管理者用
-# # URL /admin/sign_in ...
-# devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-#   sessions: "admin/sessions"
-# }
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   
+  namespace :public do
+    resources :cart_items, only: [:create, :destroy, :update, :index] 
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all'
+  end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
