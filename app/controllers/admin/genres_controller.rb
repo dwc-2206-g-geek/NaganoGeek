66e63c748091cba4fe2891e3a2e7a131271caeb0
tako0820 <105572635+tako0820@users.noreply.github.com>
2022-07-19 12:38:57 +0000

    genreコントローラー作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,7 +2,14 @@ class Admin::GenresController < ApplicationController
   def index
   end
 
+  def new
+    @genre = Genre.new
+  end
+
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_item_path(@item)
   end
   
   def edit
