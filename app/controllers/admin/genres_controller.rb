3da40bb0474ab430523d1728c022a73c01bc533f
tako0820 <105572635+tako0820@users.noreply.github.com>
2022-07-19 14:20:24 +0000

    ジャンル一覧、編集機能

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,5 +1,6 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genres = Genre.all
   end
 
   def new
@@ -9,13 +10,23 @@ class Admin::GenresController < ApplicationController
   def create
     @genre = Genre.new(genre_params)
     @genre.save
-    redirect_to admin_item_path(@item)
+    redirect_to request.referer
   end
-  
+
   def edit
+    @genre = Genre.find(params[:id])
   end
-  
+
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+  　redirect_to admin_genres_path
   end
-  
+
+  private
+
+  def genre_params
+    params.permit(:name)
+  end
+
 end
