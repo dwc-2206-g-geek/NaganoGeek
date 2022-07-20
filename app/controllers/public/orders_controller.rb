b77a3edba3e42a7c074b71b8175fa7d11d1ce365
EC2 Default User <ec2-user@ip-172-31-0-202.ap-northeast-1.compute.internal>
2022-07-20 15:55:37 +0000

    [Add] 注文views

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,40 @@
 class Public::OrdersController < ApplicationController
 
   def new
+    # @addresses = current_customer.addresses
+    @address = Address.new
+    @order = Order.new
 
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
+
+    @order = Order.new(order_params)
+  if params[:order][:address_number] == "1"
+    @order.address = current_customer.customer_address
+  elsif params[:order][:address_number] == "2"
+    if Address.exists?(name: params[:order][:registered])
+      @order.name = Address.find(params[:order][:registered]).name
+      @order.address = Address.find(params[:order][:registered]).address
+    else
+      render :new
+    end
+  elsif params[:order][:address_number] == "3"
+    address_new = current_customer.addresses.new(address_params)
+    if address_new.save
+    else
+      render :new
+    end
+  else
+    redirect_to 遷移したいページ
+  end
+  @cart_items = current_customer.cart_items.all
+  @total = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
 
   end
 
@@ -13,15 +43,34 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-
+    @order = Order.new(order_params)
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def index
-
+    @orders = Order.all
   end
 
   def show
 
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
+  end
+
+  def address_params
+    params.require(:order).permit(:postal_code, :address, :name)
   end
 
 end
