ff57c231a282a38879c049a6fe4bcd0701732f33
dmmwebcampaizawa <105425062+dmmwebcampaizawa@users.noreply.github.com>
2022-07-22 02:13:54 +0000

    “デザイン調整”

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,17 +20,18 @@ class Public::CustomersController < ApplicationController
       else
       render :edit
       end
-
   end
 
-
-
   def unsubscribe
-
+    @customer = current_customer
   end
 
   def withdraw
-
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
   end
 
     private
