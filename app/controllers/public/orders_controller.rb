e932e8925d69d5ba82d6ff04f7aecf600d9ece53
EC2 Default User <ec2-user@ip-172-31-0-202.ap-northeast-1.compute.internal>
2022-07-23 14:01:02 +0000

    [Add] 注文更新修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,16 +9,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    # @order = Order.new(order_params)
-    # @address = Address.find(params[:order][:address_id])
-    # @order.postal_code = @address.postal_code
-    # @order.address = @address.address
-    # @order.name = @address.name
-
     @order = Order.new(order_params)
+    @order.shipping_cost = 800
   if params[:order][:address_number] == "1"
-    @order.name = current_customer.first_name + current_customer.last_name
+    @order.postal_code = current_customer.postal_code
     @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
   elsif params[:order][:address_number] == "2"
     if Address.where(id: params[:order][:address_id])
       @address = Address.find(params[:order][:address_id])
@@ -31,16 +27,19 @@ class Public::OrdersController < ApplicationController
   elsif params[:order][:address_number] == "3"
     address_new = current_customer.addresses.new(address_params)
     if address_new.save
+      @order.postal_code = address_new.postal_code
+      @order.address = address_new.address
+      @order.name = address_new.name
     else
       render :new
     end
   else
-    redirect_to confirm_path
+    redirect_to new_order_path
   end
   @cart_items = current_customer.cart_items.all
   @total = 0
   @total_payment = 0
-  @order.shipping_cost = 800
+
   end
 
   def complete
@@ -50,20 +49,21 @@ class Public::OrdersController < ApplicationController
   def create
     cart_items = current_customer.cart_items.all
   @order = current_customer.orders.new(order_params)
+  @order.shipping_cost = 800
   if @order.save
     cart_items.each do |cart|
-      order_item = OrderItem.new
-      order_item.item_id = cart.item_id
-      order_item.order_id = @order.id
-      order_item.order_quantity = cart.quantity
-      order_item.order_price = cart.item.price
-      order_item.save
+      order_detail = OrderDetail.new
+      order_detail.item_id = cart.item_id
+      order_detail.order_id = @order.id
+      order_detail.amount = cart.amount
+      order_detail.price = cart.item.price
+      order_detail.save
     end
     redirect_to complete_path
     cart_items.destroy_all
   else
     @order = Order.new(order_params)
-    render :new
+    render :confirm
   end
   end
 
@@ -72,7 +72,13 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
-
+    @order = Order.find(params[:id])
+    @total = 0
+    @order.order_details.each do |order_detail|
+      @total += (order_detail.price * 1.1).floor
+    end
+    @total_payment = @total + @order.shipping_cost.to_i
+    @order_details = @order.order_details
   end
 
   private
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,4 +4,13 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
 
 
+
+  def with_tax_price
+      (price * 1.1).floor
+  end
+
+  def subtotal
+    self.with_tax_price * amount
+  end
+
 end
