b0d22f537d751d86358d7270f9db51b53b38683c
tako0820 <105572635+tako0820@users.noreply.github.com>
2022-07-24 10:51:56 +0000

    item_seeds追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,7 +3,7 @@ class Public::ItemsController < ApplicationController
     @items = Item.all.page(params[:page]).per(8)
     @genres = Genre.all
     @item_all = Item.all.where(is_active: 'true')
-    
+
   end
 
   def show
@@ -11,6 +11,6 @@ class Public::ItemsController < ApplicationController
     @genres = Genre.all
     @cart_item = CartItem.new
   end
-  
-  
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,12 @@ class Item < ApplicationRecord
   has_many :order_details
   has_one_attached :image
 
+  validates :name, presence: true
+  validates :genre_id, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  #validates :is_active, presence: true
+
   def add_tax_price
     (self.price * 1.10).floor
   end
